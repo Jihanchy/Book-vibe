130585b08ef71638fcff8bfbc39ce8c11cd81c98
Jihanchy <habibullahalquaderi2005@gmail.com>
2024-11-01 22:33:36 +0600

    added navbar and footer

---
+++ src/Components/Listed Books/ListedBooks.jsx
@@ -0,0 +1,10 @@
+
+const ListedBooks = () => {
+    return (
+        <div>
+            <h1>This book is listed</h1>
+        </div>
+    );
+};
+
+export default ListedBooks;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
+import ListedBooks from './Components/Listed Books/ListedBooks.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard></Dashboard>
+      },
+      {
+        path: '/listedBooks',
+        element: <ListedBooks></ListedBooks>
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
